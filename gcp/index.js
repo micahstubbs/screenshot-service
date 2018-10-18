3a90d4b49878f3ee7c40efc0b0b0d9382bf64242
Micah Stubbs <micah.stubbs@gmail.com>
2018-10-18 10:05:40 -0700

    add comments, wait until all content is loaded before taking screenshot

--- gcp/index.js
+++ gcp/index.js
@@ -10,16 +10,25 @@ exports.screenshot = async (req, res) => {
     )
   }
 
+  // launch headless Chrome
   const browser = await puppeteer.launch({
     args: ['--no-sandbox']
   })
+
+  // visit the page we want to screenshot
   const page = await browser.newPage()
-  await page.goto(url)
+  await page.goto(url, { waitUntil: 'networkidle0' })
+  await page.waitForNavigation({ waitUntil: 'networkidle0' })
+
+  // take a screenshot of the full page
   const imageBuffer = await page.screenshot({
     fullPage: true
   })
+
+  // close the headless browser
   await browser.close()
 
+  // send the screenshot back to the requester
   res.set('Content-Type', 'image/png')
   res.send(imageBuffer)
 }
