dd66da666e65f3ac6fdd27f327810ad644c4fae5
Micah Stubbs <micah.stubbs@gmail.com>
2018-10-18 10:49:06 -0700

    updated sample call

--- gcp/sample-call.js
+++ gcp/sample-call.js
@@ -2,4 +2,6 @@
 
 https://us-central1-blockbuilder-search.cloudfunctions.net/screenshot?path=crossfilter.png&url=https://bl.ocks.org/micahstubbs/raw/66db7c01723983ff028584b6f304a54a/
 
+https://us-central1-blockbuilder-search.cloudfunctions.net/screenshot?path=crossfilter.png&url=https://bl.ocks.org/olabi/raw/11bd8d62f975b25f23a575f9d52ed4af/
+
 */
