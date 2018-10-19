a8e97182011822868a3948662b1c9769b0a5a11c
Micah Stubbs <micah.stubbs@gmail.com>
2018-10-19 14:04:11 -0700

    add bucket copy command

--- dedicated-server/setup.sh
+++ dedicated-server/setup.sh
@@ -7,3 +7,44 @@ sudo apt-get install nodejs
 nodejs -v
 # v8.12.0
 
+# install missing dependencies for Chrome
+# https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md
+sudo apt-get install gconf-service
+sudo apt-get install libasound2
+sudo apt-get install libatk1.0-0
+sudo apt-get install libatk-bridge2.0-0
+sudo apt-get install libc6
+sudo apt-get install libcairo2
+sudo apt-get install libcups2
+sudo apt-get install libdbus-1-3
+sudo apt-get install libexpat1
+sudo apt-get install libfontconfig1
+sudo apt-get install libgcc1
+sudo apt-get install libgconf-2-4
+sudo apt-get install libgdk-pixbuf2.0-0
+sudo apt-get install libglib2.0-0
+sudo apt-get install libgtk-3-0
+sudo apt-get install libnspr4
+sudo apt-get install libpango-1.0-0
+sudo apt-get install libpangocairo-1.0-0
+sudo apt-get install libstdc++6
+sudo apt-get install libx11-6
+sudo apt-get install libx11-xcb1
+sudo apt-get install libxcb1
+sudo apt-get install libxcomposite1
+sudo apt-get install libxcursor1
+sudo apt-get install libxdamage1
+sudo apt-get install libxext6
+sudo apt-get install libxfixes3
+sudo apt-get install libxi6
+sudo apt-get install libxrandr2
+sudo apt-get install libxrender1
+sudo apt-get install libxss1
+sudo apt-get install libxtst6
+sudo apt-get install ca-certificates
+sudo apt-get install fonts-liberation
+sudo apt-get install libappindicator1
+sudo apt-get install libnss3
+sudo apt-get install lsb-release
+sudo apt-get install xdg-utils
+sudo apt-get install wget
