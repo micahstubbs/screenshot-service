552e85e1bd314a822765193e426d68f371b903cb
Micah Stubbs <micah.stubbs@gmail.com>
2018-10-19 15:14:08 -0700

    use screenshots dir

--- dedicated-server/test.js
+++ dedicated-server/test.js
@@ -9,7 +9,7 @@ const filenamify = require('filenamify')
   // const url =
   //   'https://bl.ocks.org/micahstubbs/raw/3e931f7b5876254d7156a85cdd286f7b/'
 
-  const path = `${filenamify(url, { replacement: '-' }).replace(
+  const path = `./screenshots/${filenamify(url, { replacement: '-' }).replace(
     /\./g,
     '-'
   )}.png`
