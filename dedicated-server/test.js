09fbacf6ef29df504d27d8d4b5e3d5fa2cb8d761
Micah Stubbs <micah.stubbs@gmail.com>
2018-10-19 14:24:37 -0700

    shorten timeout, use less expensive example for now

--- dedicated-server/test.js
+++ dedicated-server/test.js
@@ -1,18 +1,24 @@
 const puppetteer = require('puppeteer')
 ;(async () => {
+  // crossfilter
+  // const url =
+  //   'https://bl.ocks.org/micahstubbs/raw/66db7c01723983ff028584b6f304a54a/'
+
+  // svg line plot
   const url =
-    'https://bl.ocks.org/micahstubbs/raw/66db7c01723983ff028584b6f304a54a/'
+    'https://bl.ocks.org/micahstubbs/raw/3e931f7b5876254d7156a85cdd286f7b/'
+
   const path = `crossfilter-block.png`
   const browser = await puppetteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   })
   const page = await browser.newPage()
 
-  const fiftyMinutes = 3000000
-  await page.goto(url, { waitUntil: 'networkidle0', timeout: fiftyMinutes })
+  const oneMinute = 60000
+  await page.goto(url, { waitUntil: 'networkidle0', timeout: oneMinute })
   await page.waitForNavigation({
     waitUntil: 'networkidle0',
-    timeout: fiftyMinutes
+    timeout: oneMinute
   })
   await page.screenshot({ path, fullPage: true })
   await browser.close()
