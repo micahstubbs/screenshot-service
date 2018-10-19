9d9cc5c80572591ffaaf2c1606caf0520d21408f
Micah Stubbs <micah.stubbs@gmail.com>
2018-10-19 12:59:53 -0700

    add puppeteer config

---
+++ dedicated-server/setup.sh
@@ -0,0 +1,9 @@
+# install nodejs v8.12.0
+# How To Install Using a PPA 
+# https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-16-04
+curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get install nodejs
+nodejs -v
+# v8.12.0
+
---
+++ dedicated-server/test.js
@@ -0,0 +1,12 @@
+const puppetteer = require('puppeteer')(async () => {
+  const url =
+    'https://bl.ocks.org/micahstubbs/raw/66db7c01723983ff028584b6f304a54a/'
+  const path = `crossfilter-block.png`
+  const browser = await puppetteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  })
+  const page = await browser.newPage()
+  await page.goto(url)
+  await page.screenshot({ path })
+  await browser.close()
+})
