82fd0ef09ff2935044c4b2164a0cede71be01de5
Micah Stubbs <micah.stubbs@gmail.com>
2018-10-22 12:50:35 -0700

    hook up the streams

--- dedicated-server/cache.js
+++ dedicated-server/cache.js
@@ -6,6 +6,9 @@ const path = require('path')
 module.exports = async props => {
   const { path, filename } = props
 
+  // create a read stream
+  const src = fs.createReadStream(path)
+
   //
   // write to bucket
   //
@@ -24,8 +27,9 @@ module.exports = async props => {
   const bucket = googleCloudStorage.bucket('blockbuilder-screenshots')
 
   // Create a new blob in the bucket and upload the file data
-  const blob = bucket.file(file)
+  const blob = bucket.file(filename)
   const blobStream = blob.createWriteStream()
+  src.pipe(blobStream)
 
   blobStream.on('error', err => {
     next(err)
