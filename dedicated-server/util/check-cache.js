f3c323851e56d074b8da5eb507617be3aaf938f8
Micah Stubbs <micah.stubbs@gmail.com>
2018-10-23 11:43:39 -0700

    promise-based list files

--- dedicated-server/util/check-cache.js
+++ dedicated-server/util/check-cache.js
@@ -1,13 +1,23 @@
 const { Storage } = require('@google-cloud/storage')
 
-module.exports = async filename => {
+module.exports = filename => {
   const storage = new Storage()
   const bucketName = 'blockbuilder-screenshots'
 
-  // list files in the bucket
-  const results = await storage.bucket(bucketName).getFiles()
+  // Lists files in the bucket
+  storage
+    .bucket(bucketName)
+    .getFiles()
+    .then(results => {
+      console.log('results', results)
+      const files = results[0]
 
-  const files = results[0]
-  // return files.includes(filename)
-  return files
+      console.log('files:')
+      files.forEach(file => {
+        console.log(file.name)
+      })
+    })
+    .catch(err => {
+      console.error('ERROR:', err)
+    })
 }
--- dedicated-server/util/check-cache.test.js
+++ dedicated-server/util/check-cache.test.js
@@ -1,5 +1,5 @@
 const checkCache = require('./check-cache.js')
 
 const filename = 'crossfilter-block.png'
-const found = checkCache(filename)
-console.log(`found ${filename}? ${JSON.stringify(found, null, 2)}`)
+checkCache(filename)
+// console.log(`found ${filename}? ${JSON.stringify(found, null, 2)}`)
