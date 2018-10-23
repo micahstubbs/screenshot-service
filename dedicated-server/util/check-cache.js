5c1d3f024ef883fd49c0b9e1616b5d20d3fcd0b5
Micah Stubbs <micah.stubbs@gmail.com>
2018-10-22 17:27:57 -0700

    async await in check cache

--- dedicated-server/util/check-cache.js
+++ dedicated-server/util/check-cache.js
@@ -1,18 +1,12 @@
 const { Storage } = require('@google-cloud/storage')
 
-module.exports = filename => {
+module.exports = async filename => {
   const storage = new Storage()
   const bucketName = 'blockbuilder-screenshots'
 
   // list files in the bucket
-  storage
-    .bucket(bucketName)
-    .getFiles()
-    .then(results => {
-      const files = results[0]
-      return files.includes(filename)
-    })
-    .catch(err => {
-      console.error('ERROR', err)
-    })
+  const results = await storage.bucket(bucketName).getFiles()
+
+  const files = results[0]
+  return files.includes(filename)
 }
