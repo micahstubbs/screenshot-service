bac55b9801afb91c3d45f889dd7e9756b10a296b
Micah Stubbs <micah.stubbs@gmail.com>
2018-10-22 17:29:19 -0700

    await in test as well

--- dedicated-server/util/check-cache.test.js
+++ dedicated-server/util/check-cache.test.js
@@ -1,5 +1,5 @@
 const checkCache = require('./check-cache.js')
 
 const filename = 'crossfilter-block.png'
-const found = checkCache(filename)
+const found = await checkCache(filename)
 console.log(`found ${filename}? ${found}`)
