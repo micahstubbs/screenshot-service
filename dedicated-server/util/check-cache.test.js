f03c4fde4e1f08c8ac6ec93e76425e341d94b66d
Micah Stubbs <micah.stubbs@gmail.com>
2018-10-22 17:54:14 -0700

    try modified test

--- dedicated-server/util/check-cache.test.js
+++ dedicated-server/util/check-cache.test.js
@@ -2,4 +2,4 @@ const checkCache = require('./check-cache.js')
 
 const filename = 'crossfilter-block.png'
 const found = checkCache(filename)
-console.log(`found ${filename}? ${found}`)
+console.log(`found ${filename}? ${JSON.stringify(found, null, 2)}`)
