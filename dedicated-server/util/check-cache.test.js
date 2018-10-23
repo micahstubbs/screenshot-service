cd6a04ebd02c23c97ba014920b830c532e2fa58c
Micah Stubbs <micah.stubbs@gmail.com>
2018-10-22 17:29:45 -0700

    no await outside async function

--- dedicated-server/util/check-cache.test.js
+++ dedicated-server/util/check-cache.test.js
@@ -1,5 +1,5 @@
 const checkCache = require('./check-cache.js')
 
 const filename = 'crossfilter-block.png'
-const found = await checkCache(filename)
+const found = checkCache(filename)
 console.log(`found ${filename}? ${found}`)
