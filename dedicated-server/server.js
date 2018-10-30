b165541caff0bfcecbd35cb98eb0ea0d13831dd2
Micah Stubbs <micah.stubbs@gmail.com>
2018-10-29 21:38:24 -0700

    set request entity limit to 12mb for express body-parser

--- dedicated-server/server.js
+++ dedicated-server/server.js
@@ -14,7 +14,7 @@ const keys = require('./keys.js')
 const app = express()
 const port = 8890
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false, limit: '12mb' }))
 app.use(bodyParser.json())
 
 app.listen(port, () => {
